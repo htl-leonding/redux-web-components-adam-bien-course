14da18202731ece0b3525969ccfd3b7bd4d2d67f
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-06-24 16:31:06 +0200

    les 2. redux store creation - finished

--- events/src/app.js
+++ events/src/app.js
@@ -1,1 +1,25 @@
-console.log("Hello Web Standards");
+import { createStore } from "./lib/redux.mjs";
+
+const multiply = ( state = 21, action) => {
+    console.log(state, action);
+    if (action.type === 'M') {
+        return state * action.payload;
+    }
+    return state;
+}
+
+const store = createStore(multiply);
+
+// subscribe to changes
+store.subscribe(_ => console.log('state changed', store.getState()));
+
+const initial = store.getState();
+console.log('initial', initial);
+
+// change the value
+store.dispatch({
+    type: 'M',
+    payload: 2
+})
+
+console.log(store.getState());
