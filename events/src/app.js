e458187f0eeeefd8d3c4b99d88b33ac30fd022f7
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-06-24 16:48:39 +0200

    les. 03 - redux developer tools - finished

--- events/src/app.js
+++ events/src/app.js
@@ -8,7 +8,7 @@ const multiply = ( state = 21, action) => {
     return state;
 }
 
-const store = createStore(multiply);
+const store = createStore(multiply, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 // subscribe to changes
 store.subscribe(_ => console.log('state changed', store.getState()));
@@ -21,5 +21,10 @@ store.dispatch({
     type: 'M',
     payload: 2
 })
+store.dispatch({
+    type: 'M',
+    payload: 3
+})
+
 
 console.log(store.getState());
