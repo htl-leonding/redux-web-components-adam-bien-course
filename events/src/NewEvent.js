aaa2cd48ac757ad5b6c8fb7826ef5a4738126202
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-06-24 16:57:21 +0200

    les. 04 - first web component - finished

---
+++ events/src/NewEvent.js
@@ -0,0 +1,7 @@
+class NewEvent extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = "simple string"
+    }
+}
+
+customElements.define('a-newevent', NewEvent);
--- events/src/app.js
+++ events/src/app.js
@@ -1,30 +1,4 @@
-import { createStore } from "./lib/redux.mjs";
-
-const multiply = ( state = 21, action) => {
-    console.log(state, action);
-    if (action.type === 'M') {
-        return state * action.payload;
-    }
-    return state;
-}
-
-const store = createStore(multiply, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-// subscribe to changes
-store.subscribe(_ => console.log('state changed', store.getState()));
-
-const initial = store.getState();
-console.log('initial', initial);
-
-// change the value
-store.dispatch({
-    type: 'M',
-    payload: 2
-})
-store.dispatch({
-    type: 'M',
-    payload: 3
-})
+import './NewEvent.js'
 
 
-console.log(store.getState());
+
