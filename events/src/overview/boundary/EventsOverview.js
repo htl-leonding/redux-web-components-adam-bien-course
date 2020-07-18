8f4bc667a8fe17608907f8c363460853b7d3068e
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-07-18 07:48:51 +0200

    les. 014 - rendering a list - finished

--- events/src/overview/boundary/EventsOverview.js
+++ events/src/overview/boundary/EventsOverview.js
@@ -1,16 +1,20 @@
-import { html, render } from "../../lib/lit-html.js";
+import {html, render} from "../../lib/lit-html.js";
 import AirElement from "../../AirElement.js";
 
 
 class EventsOverview extends AirElement {
 
     view() {
+        const eventList = this.state.events;
         console.log('UPDATING +1', this.state);
         return html`
-        <h2>hello,overview</h2>
+        <ol>
+         ${eventList.map(({ name, description}) => html`
+             <li>${name} => ${description}</li>
+         `)}
+        </ol>
         `;
     }
 }
 
 customElements.define('a-events-overview', EventsOverview);
-
