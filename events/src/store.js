29fca45838da08bc17c9ca8e3103885562785864
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-07-18 10:42:45 +0200

    les. 017 - refactoring redux - finished

---
+++ events/src/store.js
@@ -0,0 +1,18 @@
+import {createStore} from "./lib/redux.mjs";
+
+const deepCopy = input => JSON.parse(JSON.stringify(input));
+
+// reducer
+const events = (state = {events:[]}, action) => {
+    const {type,payload} = action;
+    switch (type) {
+        case 'NEW_EVENT_CREATED':
+            return deepCopy({
+                events: state.events.concat(payload)
+            });
+    }
+    console.log(state, action);
+    return state;
+}
+const store = createStore(events, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default store;
