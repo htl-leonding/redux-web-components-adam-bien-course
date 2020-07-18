e86ce1e942150a786f69112abc3b1f3629aa2d36
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-07-18 13:27:32 +0200

    les. 027 - the filter custom element - finished

--- events/src/overview/boundary/EventsOverview.js
+++ events/src/overview/boundary/EventsOverview.js
@@ -1,6 +1,6 @@
 import {html, render} from "../../lib/lit-html.js";
 import AirElement from "../../AirElement.js";
-
+import '../../filter/boundary/EventsFilter.js'
 
 class EventsOverview extends AirElement {
 
@@ -8,6 +8,7 @@ class EventsOverview extends AirElement {
         const eventList = this.state.events;
         console.log('UPDATING +1', this.state);
         return html`
+        <a-events-filter></a-events-filter>
         <ol>
          ${eventList.map(({ eventname, description}) => html`
              <li>${eventname} => ${description}</li>
