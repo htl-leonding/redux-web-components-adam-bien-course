d696226c64fe0a7adfc74f39421c4051a5f2d64d
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-07-18 11:17:06 +0200

    les. 020 - deep copy refactoring - finished

--- events/src/creation/entity/EventsReducer.js
+++ events/src/creation/entity/EventsReducer.js
@@ -1,19 +1,17 @@
-const deepCopy = input => JSON.parse(JSON.stringify(input));
-
 const NEW_EVENT_CREATED = 'NEW_EVENT_CREATED';
 
 // reducer
-const events = (state = {events:[]}, action) => {
-    const {type,payload} = action;
+const events = (state = {events: []}, action) => {
+    const {type, payload} = action;
     switch (type) {
         case NEW_EVENT_CREATED:
-            return deepCopy({
+            return {
                 events: state.events.concat(payload)
-            });
+            };
     }
     console.log(state, action);
     return state;
 }
 
 export default events;
-export { NEW_EVENT_CREATED };
+export {NEW_EVENT_CREATED};
